11993d2feaedfa608a38f5b993117b31208d4585
Gonzalojp18 <pgonzalojose@gmail.com>
2023-03-04 17:48:37 -0300

    [gonzalo][add] Scroll Menu and querys

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const loading = document.getElementById("loading");
     loading.style.display = "flex";
 });
+
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector(".sticky");
+    if (window.pageYOffset > nav.offsetTop + 700) {
+        nav.classList.add("NavScroll");
+        nav.style.visibility = 'visible';
+    } else {
+        nav.classList.remove("NavScroll");
+        nav.style.visibility = 'hidden'
+    }
+});
